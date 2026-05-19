34dd21d58a02468126595c38de7d10240f3a692f
Le0X8 <84378319+Le0X8@users.noreply.github.com>
2026-05-19 16:58:01 +0200

    mehr reactions

--- src/reactions.ts
+++ src/reactions.ts
@@ -2,4 +2,16 @@ export const reactions = [
   ['big yahu', '🇮🇱'],
   ['000 jahr', '🇮🇱'],
   ['aura', '🅰️', '🇺', '🇷', '🇦'],
+
+  ['fearless', '<:fearless:241062021695537153>'],
+  ['speak now', '<:speaknow:241061975553998849>'],
+  [' red', '<:red:241061922986786817>'],
+  ['1989', '<:1989:241061862756581378>'],
+  ['reputation', '<:reputation:350017146652459019>'],
+  ['lover', '<:lover:588826600402059290>'],
+  ['folklore', '<:folklore:735835771570749450>'],
+  ['evermore', '<:evermore:786589893483692032>'],
+  ['midnights', '<:midnights:1013663160970002512>'],
+  ['tortured poets', '<:torturedpoetsdept:1203932365706235955>'],
+  ['showgirl', '<:thelifeofashowgirl:1405325530315755663>'],
 ];
