185e21c6211284d6b0ccab2288707e63ef554cfb
Le0X8 <84378319+Le0X8@users.noreply.github.com>
2026-05-20 20:23:37 +0200

    fixed banane list rendering

--- src/commands/fun/bananen.ts
+++ src/commands/fun/bananen.ts
@@ -1,12 +1,15 @@
 import { Command } from '$commands';
 import { Banane, bananeStrings, bananeValues } from '@/commands/debug/error.ts';
 import { buildEmbed } from '@/lib/embeds/default-embed.ts';
+import { ApplicationCommandOptionType } from 'discord.js';
 
 export default new Command(
   'bananen',
   'Zeigt dir deine Bananen an',
   async (interaction) => {
-    const id = interaction.user.id;
+    const user = interaction.options.getUser('user', false) || interaction.user;
+
+    const id = user.id;
 
     const bananen: Record<Banane, number> = store.get(id, 'banane') ?? {};
     const value = Object.entries(bananen).reduce(
@@ -18,19 +21,28 @@ export default new Command(
     await interaction.reply({
       embeds: [
         await buildEmbed(
-          `Bananen von <@${id}>`,
+          `Alle Bananen von @${user.username}`,
           value == 0 ? 'Dieser Nutzer hat noch keine Bananen gesammelt.' : null,
           Object.entries(bananen).map(([key, count]) => {
             const banane = parseInt(key) as Banane;
             const strings = bananeStrings(banane);
             return [
               `${strings[1]} **${strings[0]}**: ${count}`,
-              `${count}x ${strings[1]}`,
+              `\`${count}x\` Bananen @ \`${bananeValues[banane]}nb\` = \`${count * bananeValues[banane]}nb\``,
             ];
           }),
-          value == 0 ? null : `Summe: Wert von **${value}** normalen Bananen`,
+          value == 0 ? null : `Summe: Wert von ${value} normalen Bananen (nb)`,
         ),
       ],
     });
   },
+  false,
+  [
+    {
+      name: 'user',
+      description: 'Wessen Bananen möchtest du sehen?',
+      type: ApplicationCommandOptionType.User,
+      required: false,
+    },
+  ],
 );
