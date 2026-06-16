1f668954525db9335841617457486a7fe50a36fd
Le0X8 <84378319+Le0X8@users.noreply.github.com>
2026-06-16 11:55:26 +0200

    added new banana edit owner cmd

---
+++ src/commands/owner/editcash.ts
@@ -0,0 +1,95 @@
+import { Command } from '$commands';
+import config from '$config' with { type: 'json' };
+import { Banane, bananeValues } from '@/commands/debug/error.ts';
+import { ApplicationCommandOptionType } from 'discord.js';
+
+export default new Command(
+  'zzz-owner-editcash',
+  '[Owner-exclusive] Bearbeitet Bananen',
+  async (interaction) => {
+    if (interaction.user.id !== config.owner) {
+      await interaction.reply({
+        content: 'You are not permitted to use this command.',
+        ephemeral: true,
+      });
+      return;
+    }
+
+    const user = interaction.options.getUser('user', true);
+    const property = interaction.options.getString('property', true);
+    const value = interaction.options.getInteger('value', false);
+
+    let balance: Record<Banane, number> = store.get(user.id, 'banane') ?? {};
+    const donators: Record<string, number> = store.get('donators') ?? {};
+    const invested = donators[user.id] ?? 0;
+    const cash = Object.entries(balance).reduce(
+      (acc, [key, count]) =>
+        acc + (bananeValues[parseInt(key) as Banane] ?? 0) * count,
+      0,
+    );
+
+    switch (property) {
+      case 'invested':
+        if (typeof value != 'number') {
+          await interaction.reply({
+            content: `Dieser Nutzer hat aktuell ${invested} investierte Bananen.`,
+            ephemeral: true,
+          });
+          return;
+        }
+        donators[user.id] = value;
+        store.set('donators', null, donators);
+        await interaction.reply(
+          `Investierte Bananen von ${user.username} wurden auf ${value} gesetzt.`,
+        );
+        break;
+      case 'cash':
+        if (typeof value != 'number') {
+          await interaction.reply({
+            content: `Dieser Nutzer hat aktuell ${cash} Bananen.`,
+            ephemeral: true,
+          });
+          return;
+        }
+        balance = {
+          [Banane.Gelb]: value,
+        } as unknown as Record<Banane, number>;
+        store.set(user.id, 'banane', balance);
+        await interaction.reply(
+          `Bananen von ${user.username} wurden auf ${value} gesetzt.`,
+        );
+        break;
+    }
+  },
+  false,
+  [
+    {
+      name: 'user',
+      description: 'user',
+      type: ApplicationCommandOptionType.User,
+      required: true,
+    },
+    {
+      name: 'property',
+      description: 'property',
+      type: ApplicationCommandOptionType.String,
+      required: true,
+      choices: [
+        {
+          name: 'Invested',
+          value: 'invested',
+        },
+        {
+          name: 'Cash',
+          value: 'cash',
+        },
+      ],
+    },
+    {
+      name: 'value',
+      description: 'value',
+      type: ApplicationCommandOptionType.Integer,
+      required: false,
+    },
+  ],
+);
