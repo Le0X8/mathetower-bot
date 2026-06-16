5a1140b8c64d96e42dcb1142713d0781c6d46999
Le0X8 <84378319+Le0X8@users.noreply.github.com>
2026-06-16 13:13:53 +0200

    gifting verbessert

--- src/commands/fun/gift.ts
+++ src/commands/fun/gift.ts
@@ -1,7 +1,7 @@
 import { Command } from '$commands';
 import { Banane, bananeValues } from '@/commands/debug/error.ts';
 import { nb } from '@/lib/helpers/bananen.ts';
-import { ApplicationCommandOptionType } from 'discord.js';
+import { ApplicationCommandOptionType, Role, User } from 'discord.js';
 import config from '$config' with { type: 'json' };
 
 export default new Command(
@@ -9,13 +9,25 @@ export default new Command(
   'Verschenkt Bananen',
   async (interaction) => {
     const sender = interaction.user;
-    const receiver = interaction.options.getUser('user', true);
+    const receiver = interaction.options.getMentionable('user', true);
     let amount = interaction.options.getInteger('amount', true);
+
+    const receivers = [];
+
+    if (receiver instanceof Role) {
+      const members = await interaction.guild?.members.fetch();
+      if (members)
+        members.forEach((member) => {
+          if (member.roles.cache.has(receiver.id)) receivers.push(member.id);
+        });
+    }
+    if (receiver instanceof User) receivers.push(receiver.id);
 
     const senderBalance: Record<Banane, number> =
       store.get(sender.id, 'banane') ?? {};
-    const receiverBalance: Record<Banane, number> =
-      store.get(receiver.id, 'banane') ?? {};
+    const receiverBalances: Record<Banane, number>[] = receivers.map((r) =>
+      store.get(r, 'banane'),
+    ) ?? [{}];
 
     let senderTotal = Object.entries(senderBalance).reduce(
       (acc, [key, count]) =>
@@ -25,6 +37,9 @@ export default new Command(
 
     if (amount == -1) amount = senderTotal; // intentional overflow
 
+    const singleAmount = amount;
+    amount = amount * receivers.length;
+
     if (senderTotal < amount || amount < 1) {
       await interaction.reply({
         content: `Du hast nicht genug Bananen, um \`${nb(amount)}\` zu verschenken! Dein aktueller Kontostand beträgt \`${nb(senderTotal)}\`.`,
@@ -33,34 +48,38 @@ export default new Command(
       return;
     }
 
-    if (receiver.id == config.uid) {
-      const donators: Record<string, number> = store.get('donators') ?? {};
-      donators[sender.id] = (donators[sender.id] ?? 0) + amount;
-      store.set('donators', null, donators);
-      const total = Object.values(donators).reduce((a, b) => a + b, 0);
-      const part = ((donators[sender.id] / total) * 50).toFixed(2);
+    const msgs: string[] = [];
+    receivers.forEach((r, i) => {
+      if (r == config.uid) {
+        const donators: Record<string, number> = store.get('donators') ?? {};
+        donators[sender.id] = (donators[sender.id] ?? 0) + singleAmount;
+        store.set('donators', null, donators);
+        const total = Object.values(donators).reduce((a, b) => a + b, 0);
+        const part = ((donators[sender.id] / total) * 50).toFixed(2);
 
-      await interaction.reply(
-        `${sender} hat \`${nb(amount)}\` an ${receiver} gegeben.\nDanke!\n\nDu bekommst jetzt ${part}% Anteil an meinem Ertrag.`,
-      );
-    } else
-      await interaction.reply(
-        `${sender} hat \`${nb(amount)}\` an ${receiver} gegeben.`,
-      );
+        msgs.push(
+          `${sender} hat \`${nb(singleAmount)}\` an <@${r}> gegeben.\n-# Danke!\n-# Du bekommst jetzt ${part}% Anteil an meinem Ertrag.`,
+        );
+      } else
+        msgs.push(`${sender} hat \`${nb(singleAmount)}\` an <@${r}> gegeben.`);
+
+      receiverBalances[i][Banane.Gelb] =
+        (receiverBalances[i][Banane.Gelb] ?? 0) + singleAmount;
+
+      store.set(r, 'banane', receiverBalances[i]);
+    });
 
     senderBalance[Banane.Verkauft] =
       (senderBalance[Banane.Verkauft] ?? 0) + amount;
-    receiverBalance[Banane.Gelb] = (receiverBalance[Banane.Gelb] ?? 0) + amount;
-
     store.set(sender.id, 'banane', senderBalance);
-    store.set(receiver.id, 'banane', receiverBalance);
+    await interaction.reply(msgs.join('\n'));
   },
   false,
   [
     {
       name: 'user',
       description: 'Empfänger der Bananen',
-      type: ApplicationCommandOptionType.User,
+      type: ApplicationCommandOptionType.Mentionable,
       required: true,
     },
     {
