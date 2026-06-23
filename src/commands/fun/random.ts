9856af9b742de023050180be5bfcec80744760fa
Le0X8 <84378319+Le0X8@users.noreply.github.com>
2026-06-23 10:30:49 +0200

    better random oder so

--- src/commands/fun/random.ts
+++ src/commands/fun/random.ts
@@ -1,10 +1,40 @@
 import { Command } from '$commands';
 
+const vocals = ['a', 'e', 'i', 'o', 'u'];
+const consonants = [
+  'b',
+  'c',
+  'd',
+  'f',
+  'g',
+  'h',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
 export default new Command('random', 'random halt', async (interaction) => {
   const length = Math.floor(Math.random() * 7) + 3;
-  const randomString = Array.from({ length }, () =>
-    String.fromCharCode(Math.floor(Math.random() * 26) + 97),
-  ).join('');
+  let vocal = false;
+  const randomString = Array.from({ length }, (_) => {
+    if (Math.random() < 0.5) vocal = !vocal;
+    vocal = !vocal;
+    return vocal
+      ? consonants[Math.floor(Math.random() * consonants.length)]
+      : vocals[Math.floor(Math.random() * vocals.length)];
+  }).join('');
 
   await interaction.reply(randomString);
 });
