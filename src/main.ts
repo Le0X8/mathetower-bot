7ffcb761450b18928fdf587bf3c6747ce06c64f2
Le0X8 <84378319+Le0X8@users.noreply.github.com>
2026-07-14 11:25:13 +0200

    hoffentlich fix

--- src/main.ts
+++ src/main.ts
@@ -196,14 +196,13 @@ client.on(Events.MessageCreate, async (message) => {
   try {
     if (message.author.bot) return;
 
+    appendFileSync('./dataset.txt', message.content + '\n');
     specialMessages(message).catch(console.error);
   } catch {}
 });
 
 async function specialMessages(message: Message<boolean>) {
   const content = message.content.toLowerCase();
-
-  appendFileSync('./dataset.txt', message.content + '\n');
 
   if (
     !content.includes('!novx') &&
